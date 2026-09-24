3e73aac9c553ddace19d4cb5e4098ead655fe50a
knov35 <musictasin@gmail.com>
2026-09-24 22:33:17 +0600

    removed various ai test files

--- backend/test_connection.js
+++
@@ -1,7 +0,0 @@
-const pool = require('./db');
-
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) console.error('FAILED:', err.message);
-  else console.log('CONNECTED:', res.rows[0].now);
-  pool.end();
-});
